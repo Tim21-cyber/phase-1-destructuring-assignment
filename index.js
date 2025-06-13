ff47ce6a2eae8177a9d20e43c402ae19777e8b99
Timothy <timothy.ochieng1@student.moringaschool.com>
2025-06-13 15:45:00 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 const farmAnimals = 'cow horse sheep pig chicken';
+const [moo, neigh, baa, oink, cluck] = farmAnimals.split(' ');
+const [bessie, dolly, babe, little] = ['cow', 'sheep', 'pig', 'chicken'];
+const [blackAndWhite, black, pink] = ['cow', 'sheep', 'pig'];
+
 
 const colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'];
+const [red, orange, yellow, green, blue, indigo, violet] = colors;
+const [r, o, y, g, b, v] = ['red', 'orange', 'yellow', 'green', 'blue', 'violet'];
+const [indg] = ['indigo'];
 
 const muppet = {
   muppetName: 'Miss Piggy',
@@ -9,6 +16,7 @@ const muppet = {
   job: 'Cast member of The Muppet Show',
   partner: 'Kermit'
 };
+const { muppetName, color, song, job, partner } = muppet;
 
 const nestedMuppet = {
   nestedName: 'Kermit',
@@ -24,6 +32,11 @@ const nestedMuppet = {
   nestedJob: 'Host of The Muppet Show',
   nestedPartner: 'Miss Piggy'
 };
+const { 
+  album: { theMuppetMovie: { song2, song4 } }, 
+  nestedJob, 
+  nestedPartner 
+} = nestedMuppet;
 
 // Strings
 
